dc2585a3d1cc75f048dd27cfab230af097bae49a
Sergio <santossergiorealbjj@outlook.com>
2025-11-15 08:32:57 -0300

    Update io_utils.py

--- libs/io_utils.py
+++ libs/io_utils.py
@@ -1,8 +1,10 @@
 # Biblioteca: IO Utils
 # Funções auxiliares para leitura e escrita
 
+
 def read_csv(spark, path):
     return spark.read.format("csv").option("header", "true").load(path)
+
 
 def write_parquet(df, path):
     df.write.mode("overwrite").parquet(path)
