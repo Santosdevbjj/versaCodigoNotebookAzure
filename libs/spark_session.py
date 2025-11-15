36129cec012d53ea93d13edef7fdf6b04d4052da
Sergio <santossergiorealbjj@outlook.com>
2025-11-15 06:38:31 -0300

    Create spark_session.py

---
+++ libs/spark_session.py
@@ -0,0 +1,7 @@
+# Biblioteca: Spark Session
+# Inicialização da sessão Spark
+
+from pyspark.sql import SparkSession
+
+def get_spark_session(app_name="DatabricksApp"):
+    return SparkSession.builder.appName(app_name).getOrCreate()
