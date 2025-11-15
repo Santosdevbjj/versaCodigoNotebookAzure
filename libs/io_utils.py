6d1e56dfdfee503ae34fc02073e6a03300fed8ca
Sergio <santossergiorealbjj@outlook.com>
2025-11-15 06:36:22 -0300

    Create io_utils.py

---
+++ libs/io_utils.py
@@ -0,0 +1,8 @@
+# Biblioteca: IO Utils
+# Funções auxiliares para leitura e escrita
+
+def read_csv(spark, path):
+    return spark.read.format("csv").option("header", "true").load(path)
+
+def write_parquet(df, path):
+    df.write.mode("overwrite").parquet(path)
