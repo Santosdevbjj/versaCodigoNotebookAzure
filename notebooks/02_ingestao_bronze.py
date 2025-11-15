189840838052675b0bdd3ab93136cafbfbd37580
Sergio <santossergiorealbjj@outlook.com>
2025-11-15 06:28:27 -0300

    Create 02_ingestao_bronze.py

---
+++ notebooks/02_ingestao_bronze.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# Notebook: Ingestão Bronze
+# Objetivo: Ingestão de dados brutos na camada Bronze
+
+from pyspark.sql import functions as F
+
+# MAGIC %md
+# MAGIC ## Ingestão Bronze
+# MAGIC Carregando dados CSV para a camada Bronze.
+
+dbutils.widgets.text("source_path", "/mnt/dados")
+source_path = dbutils.widgets.get("source_path")
+
+df = spark.read.format("csv").option("header", "true").load(source_path)
+df_bronze = df.withColumn("ingestion_ts", F.current_timestamp())
+
+display(df_bronze)
